42a174c3a633b1631202e528e23c03a8b688b3cd
Hilman <hilman@alphatech.id>
2019-03-24 18:00:48 +0700

    rubah port

--- app.py
+++ app.py
@@ -32,10 +32,10 @@ if __name__ == '__main__':
         elif sys.argv[1] == 'db':
             manager.run()
         else :
-            app.run(debug=False, host='0.0.0.0', port=5000)
+            app.run(debug=False, host='0.0.0.0', port=5002)
             
     except Exception as e:
-        app.run(debug=True, host='0.0.0.0', port=5000)
+        app.run(debug=True, host='0.0.0.0', port=5002)
 
 
 
